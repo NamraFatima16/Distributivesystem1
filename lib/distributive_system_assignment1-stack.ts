8dfc92ca449cc1558db6fa8a5694105fea1ffc59
Namra Fatima <20094326@mail.wit.ie>
2025-03-21 18:30:34 +0000

    addition of lambda funtions for each API endpoint

--- lib/distributive_system_assignment1-stack.ts
+++ lib/distributive_system_assignment1-stack.ts
@@ -3,11 +3,13 @@ import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as path from 'path';
 
 export class DistributiveSystemAssignment1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    
     const booksTable = new dynamodb.Table(this, 'BooksTable', {
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       partitionKey: { name: 'Author', type: dynamodb.AttributeType.STRING },
@@ -16,5 +18,23 @@ export class DistributiveSystemAssignment1Stack extends cdk.Stack {
       tableName: 'Books',
     });
 
+    const addBookLambda = new lambdanode.NodejsFunction(this, 'AddBookLambda', {
+      entry: path.join(__dirname, '../lambdas/addBook.ts'),
+      handler: 'handler',
+      environment: { TABLE_NAME: booksTable.tableName },
+       });
+
+       const getBooksLambda = new lambdanode.NodejsFunction(this, 'GetBooksLambda', {
+        entry: path.join(__dirname, '../lambdas/getBooks.ts'),
+        handler: 'handler',
+        environment: { TABLE_NAME: booksTable.tableName },
+      });
+      
+      const updateBookLambda = new lambdanode.NodejsFunction(this, 'UpdateBookLambda', {
+        entry: path.join(__dirname, '../lambdas/updateBook.ts'),
+        handler: 'handler',
+        environment: { TABLE_NAME: booksTable.tableName },
+      });
+      
   }
 }
