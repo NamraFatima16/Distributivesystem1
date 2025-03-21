7df55b66a40d33abd3db45844a568b72fcd56ed3
Namra Fatima <20094326@mail.wit.ie>
2025-03-21 17:15:19 +0000

    Initial commit

---
+++ test/distributive_system_assignment1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DistributiveSystemAssignment1 from '../lib/distributive_system_assignment1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/distributive_system_assignment1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DistributiveSystemAssignment1.DistributiveSystemAssignment1Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
