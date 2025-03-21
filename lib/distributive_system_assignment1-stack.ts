478af102b887ace5878f0f6f7a89024d1fe37579
Namra Fatima <20094326@mail.wit.ie>
2025-03-21 20:57:19 +0000

    addition of translate resourses and and IAM permissions

--- lib/distributive_system_assignment1-stack.ts
+++ lib/distributive_system_assignment1-stack.ts
@@ -4,12 +4,16 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as path from 'path';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as translate from 'aws-cdk-lib/aws-translate';
+
 
 export class DistributiveSystemAssignment1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    
+
     const booksTable = new dynamodb.Table(this, 'BooksTable', {
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       partitionKey: { name: 'Author', type: dynamodb.AttributeType.STRING },
@@ -22,19 +26,57 @@ export class DistributiveSystemAssignment1Stack extends cdk.Stack {
       entry: path.join(__dirname, '../lambdas/addBook.ts'),
       handler: 'handler',
       environment: { TABLE_NAME: booksTable.tableName },
-       });
+    });
 
-       const getBooksLambda = new lambdanode.NodejsFunction(this, 'GetBooksLambda', {
-        entry: path.join(__dirname, '../lambdas/getBooks.ts'),
-        handler: 'handler',
-        environment: { TABLE_NAME: booksTable.tableName },
-      });
-      
-      const updateBookLambda = new lambdanode.NodejsFunction(this, 'UpdateBookLambda', {
-        entry: path.join(__dirname, '../lambdas/updateBook.ts'),
-        handler: 'handler',
-        environment: { TABLE_NAME: booksTable.tableName },
-      });
-      
+    const getBooksLambda = new lambdanode.NodejsFunction(this, 'GetBooksLambda', {
+      entry: path.join(__dirname, '../lambdas/getBooks.ts'),
+      handler: 'handler',
+      environment: { TABLE_NAME: booksTable.tableName },
+    });
+
+    const updateBookLambda = new lambdanode.NodejsFunction(this, 'UpdateBookLambda', {
+      entry: path.join(__dirname, '../lambdas/updateBook.ts'),
+      handler: 'handler',
+      environment: { TABLE_NAME: booksTable.tableName },
+    });
+
+    const translateBookLambda = new lambdanode.NodejsFunction(this, 'TranslateBookLambda', {
+      entry: path.join(__dirname, '../lambdas/translateBook/index.ts'),
+      handler: 'handler',
+      environment: { TABLE_NAME: booksTable.tableName, REGION: this.region },
+    });
+
+    // granting permissions
+    booksTable.grantReadWriteData(addBookLambda);
+    booksTable.grantReadData(getBooksLambda);
+    booksTable.grantReadWriteData(updateBookLambda);
+    booksTable.grantReadWriteData(translateBookLambda);
+    translateBookLambda.addToRolePolicy(new iam.PolicyStatement({
+      actions: ['translate:TranslateText'],
+      resources: ['*'],
+    }));
+
+
+
+
+    //api gateway implementation 
+    const api = new apigateway.RestApi(this, 'BooksApi');
+    const booksResource = api.root.addResource('books');
+    const authorResource = booksResource.addResource('{author}');
+    const bookResource = authorResource.addResource('{isbn}');
+
+    booksResource.addMethod('POST', new apigateway.LambdaIntegration(addBookLambda));
+    authorResource.addMethod('GET', new apigateway.LambdaIntegration(getBooksLambda), {
+      requestParameters: { 'method.request.querystring.genre': true },
+    });
+    bookResource.addMethod('PUT', new apigateway.LambdaIntegration(updateBookLambda));
+    translationResource.addMethod('GET', new apigateway.LambdaIntegration(translateBookLambda), {
+      requestParameters: { 'method.request.querystring.language': true },
+    });
+
+
+    // implementing the API key here ???
+
+
   }
 }
