b8bf4ebe8cda79e4f6ae7930296a275e8854bc60
Namra Fatima <20094326@mail.wit.ie>
2025-03-21 18:03:51 +0000

    Adding table declaration

--- lib/distributive_system_assignment1-stack.ts
+++ lib/distributive_system_assignment1-stack.ts
@@ -1,16 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class DistributiveSystemAssignment1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const booksTable = new dynamodb.Table(this, 'BooksTable', {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: 'Author', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'ISBN', type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: 'Books',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DistributiveSystemAssignment1Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
