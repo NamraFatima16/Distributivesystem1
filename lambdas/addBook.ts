fad69a2ca7093266a3ae83813ae57d9971d3e11d
Namra Fatima <20094326@mail.wit.ie>
2025-03-23 17:31:08 +0000

    improving the addbook

--- lambdas/addBook.ts
+++ lambdas/addBook.ts
@@ -1,54 +1,48 @@
-import { Handler } from "aws-lambda";
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 
 const ddbDocClient = createDDbDocClient();
 
-export const handler: Handler = async (event, context) => {
-  try {
-    console.log("Event: ", JSON.stringify(event));
-    const requestBody = JSON.parse(event.body || '{}');
+export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
 
-    if (!requestBody.Author || !requestBody.ISBN || !requestBody.Title) {
-      return {
-        statusCode: 400,
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({ Message: "Missing required fields (Author, ISBN, Title)" }),
-      };
+  console.log("[EVENT]", JSON.stringify(event));
+  const body = event.body ? JSON.parse(event.body) : undefined;
+  if (!body) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Missing request body" })
     }
+  }
 
+  // API gateway model should take care of this wtih the bookmodel
+  if (!body?.Genre || !body.ISBN) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Missing required fields in the request body (e.g., Genre, ISBN)" })
+    }
+  }
+  
+  //what happens if there is already the same part+sort key item in db?
+  try {
     const commandOutput = await ddbDocClient.send(
       new PutCommand({
         TableName: process.env.TABLE_NAME,
-        Item: requestBody,
+        Item: body,
       })
-    );
-
-    const body = {
-      message: "Book added successfully",
-    };
-
+    )
     return {
       statusCode: 201,
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(body),
-    };
-
+      body: JSON.stringify({ message: "Book added" })
+    }
   } catch (error: any) {
-    console.log(JSON.stringify(error));
+    console.log("Error adding books: ", JSON.stringify(error));
     return {
       statusCode: 500,
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({ error }),
-    };
+      body: JSON.stringify({ error: "Could not add book" })
+    }
   }
-};
+}
 
 function createDDbDocClient() {
   const ddbClient = new DynamoDBClient({ region: process.env.REGION });
