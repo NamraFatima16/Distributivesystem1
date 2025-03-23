5e68ba1fe447a21d9daa0807174146d9b45dd80f
Namra Fatima <20094326@mail.wit.ie>
2025-03-23 18:05:05 +0000

    improving the code in the stack. configures an api gateway with security control

--- lib/distributive_system_assignment1-stack.ts
+++ lib/distributive_system_assignment1-stack.ts
@@ -1,17 +1,13 @@
-import * as cdk from 'aws-cdk-lib';
-import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import * as custom from "aws-cdk-lib/custom-resources";
-import { AwsCustomResource, PhysicalResourceId,AwsCustomResourcePolicy}from "aws-cdk-lib/custom-resources";
-import * as path from 'path';
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import { BatchWriteItemCommand } from '@aws-sdk/client-dynamodb';
 import { books } from '../seed/books';
 import { generateBatch } from '../shared/util';
-import { access } from 'fs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as custom from "aws-cdk-lib/custom-resources";
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as path from 'path';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+
 
 export class DistributiveSystemAssignment1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -48,83 +44,69 @@ export class DistributiveSystemAssignment1Stack extends cdk.Stack {
       })
     });
 
-    // const addBookLambda = new lambdanode.NodejsFunction(this, 'AddBookLambda', {
-    //   entry: path.join(__dirname, '../lambdas/addBook.ts'),
-    //   handler: 'handler',
-    //   environment: { TABLE_NAME: booksTable.tableName },
-    // });
 
-    const getBooksLambda = new lambdanode.NodejsFunction(this, 'GetBooksLambda', {
-      entry: path.join(__dirname, '../lambdas/getBooksByISBN.ts'),
+
+    //Lambda funtion integration
+    const getBooksByGenreFn = new lambda.NodejsFunction(this, 'GetBooksByGenreLambda', {
+      entry: path.join(__dirname, '../lambdas/getBooksByGenre.ts'),
       handler: 'handler',
-      environment: { TABLE_NAME: booksTable.tableName },
-      timeout: cdk.Duration.seconds(10),
-      runtime: lambda.Runtime.NODEJS_22_X
+      environment: {
+        TABLE_NAME: booksTable.tableName
+      }
+    })
+
+
+
+    // permissions for lambda funtions
+    booksTable.grantReadData(getBooksByGenreFn)
+
+
+
+
+    //Api gateway implementation
+    const apiGW = new apigateway.RestApi(this, 'BooksApi', {
+      restApiName: "BooksAppApi",
+      deployOptions: {
+        stageName: "dev",
+        loggingLevel: apigateway.MethodLoggingLevel.INFO,
+        dataTraceEnabled: true
+      },
+      defaultCorsPreflightOptions: {
+        allowHeaders: ["Content-Type", "X-Amz-Date"],
+        allowMethods: ["GET", "POST", "PUT"],
+        allowCredentials: true,
+        allowOrigins: ["*"],
+      },
+      apiKeySourceType: apigateway.ApiKeySourceType.HEADER
     });
 
-    // const updateBookLambda = new lambdanode.NodejsFunction(this, 'UpdateBookLambda', {
-    //   entry: path.join(__dirname, '../lambdas/updateBook.ts'),
-    //   handler: 'handler',
-    //   environment: { TABLE_NAME: booksTable.tableName },
-    // });
-
-    // const translateBookLambda = new lambdanode.NodejsFunction(this, 'TranslateBookLambda', {
-    //   entry: path.join(__dirname, '../lambdas/translateBook.ts'),
-    //   handler: 'handler',
-    //   environment: { TABLE_NAME: booksTable.tableName, REGION: this.region },
-    // });
-
-    // // granting permissions
-    // booksTable.grantReadWriteData(addBookLambda);
-     booksTable.grantReadData(getBooksLambda);
-    // booksTable.grantReadWriteData(updateBookLambda);
-    // booksTable.grantReadWriteData(translateBookLambda);
-    // translateBookLambda.addToRolePolicy(new iam.PolicyStatement({
-    //   actions: ['translate:TranslateText'],
-    //   resources: ['*'],
-    // }));
 
 
-    //api gateway implementation 
+    const bookApiKey = new apigateway.ApiKey(this, "addBookApiKey", {
+      description: 'Api key for POST and PUT methods to write and update books in DB',
+      enabled: true
+    })
 
-    const api = new apigateway.RestApi(this, 'BooksApi');
-    // const booksResource = api.root.addResource('books');
-    // const authorResource = booksResource.addResource('{author}');
-    const bookResource = api.root.addResource('{isbn}');
-    // const translationResource = bookResource.addResource('translation');
+    const usagePlan = new apigateway.UsagePlan(this, 'BookApiUsagePlan', {
+      name: 'apiUsagePlan',
+      description: 'Usage plan for the book API',
+      throttle: {
+        rateLimit: 1000,
+        burstLimit: 200,
+      },
+      apiStages: [
+        {
+          api: apiGW,
+          stage: apiGW.deploymentStage
+        }
+      ]
+    });
+    usagePlan.addApiKey(bookApiKey)
 
 
-    // booksResource.addMethod('POST', new apigateway.LambdaIntegration(addBookLambda), {
-    //   apiKeyRequired: true,
-    // });
-    bookResource.addMethod('GET', new apigateway.LambdaIntegration(getBooksLambda));
-
-    // bookResource.addMethod('PUT', new apigateway.LambdaIntegration(updateBookLambda), {
-    //   apiKeyRequired: true,
-    // });
-    // translationResource.addMethod('GET', new apigateway.LambdaIntegration(translateBookLambda), {
-    //   requestParameters: { 'method.request.querystring.language': true },
-    // });
-
-
-    // // implementing the API key 
-
-    // const apiKey = api.addApiKey('BooksApiKey');
-    // const usagePlan = api.addUsagePlan('BooksUsagePlan', {
-    //   name: 'BookUsagePlan',
-    //   throttle: {
-    //     rateLimit: 10,
-    //     burstLimit: 2,
-    //   }
-    // });
-
-    // usagePlan.addApiStage({
-    //   stage: api.deploymentStage,
-    // });
- new cdk.CfnOutput(this,"output",{
-  value: api.url,
-  description: 'url to access the api gateway'
- });
-
+    new cdk.CfnOutput(this, 'BookApiKeyId', {
+      value: bookApiKey.keyId,
+      description: 'ID of the API key for accessing book endpoints',
+    });
   }
 }
